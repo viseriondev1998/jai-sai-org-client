99669268d4c2863a647516362e7cbd111e295b35
mistryaksh <162544797+viseriondev1998@users.noreply.github.com>
2024-03-29 19:09:40 +0530

    update backend link

--- src/core/utils/server.utils.ts
+++ src/core/utils/server.utils.ts
@@ -1,5 +1,5 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const UserReduxSetup = fetchBaseQuery({
-baseUrl: "https://om-shanti-walfare-trust-backend-o06r.onrender.com/api/1.0",
+     baseUrl: "https://jai-sai-org-backend.onrender.com/api/1.0",
 });
